baa1923199b1dbdd60dc309ac9bd1e2ed5416411
Meellaad <meellaadeshoon@outlook.com>
2024-08-26 20:59:12 +0330

    update paths in Popular.jsx

--- src/components/Populars.jsx
+++ src/components/Populars.jsx
@@ -7,10 +7,10 @@ const Populars = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PopSeries />}></Route>
-        <Route path="/:id" element={<PopSeries />}></Route>
-        <Route path="/movies" element={<PopMovies />}></Route>
-        <Route path="/movies/:id" element={<PopMovies />}></Route>
+        <Route path="/Filimo-Website/" element={<PopSeries />}></Route>
+        <Route path="/Filimo-Website/:id" element={<PopSeries />}></Route>
+        <Route path="/Filimo-Website/movies" element={<PopMovies />}></Route>
+        <Route path="/Filimo-Website/movies/:id" element={<PopMovies />}></Route>
       </Routes>
     </BrowserRouter>
   );
