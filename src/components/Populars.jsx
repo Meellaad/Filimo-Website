ff342ebc442665fe175c1925650908230497f4b1
Milad Pourvali <107677311+Meellaad@users.noreply.github.com>
2024-12-09 22:57:01 +0330

    Update Populars.jsx

--- src/components/Populars.jsx
+++ src/components/Populars.jsx
@@ -7,10 +7,10 @@ const Populars = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="./" element={<PopSeries />}></Route>
-        <Route path="./:id" element={<PopSeries />}></Route>
-        <Route path="./movies" element={<PopMovies />}></Route>
-        <Route path="./movies/:id" element={<PopMovies />}></Route>
+        <Route path="/" element={<PopSeries />}></Route>
+        <Route path="/:id" element={<PopSeries />}></Route>
+        <Route path="/movies" element={<PopMovies />}></Route>
+        <Route path="/movies/:id" element={<PopMovies />}></Route>
       </Routes>
     </BrowserRouter>
   );
